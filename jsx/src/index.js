6a1a36447aef4cc92e13592ed893402cf080eb3f
ebeetiobhio <ebe.etiobhio@and.digital>
2023-12-18 11:28:16 +0000

    Working with Props

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -7,18 +7,10 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 //4) Create a component
 function App() {
-  let message = "Bye there!";
-  if (Math.random() > 0.5) {
-    message = "Hello there!";
-  }
-  const name = "Ebe";
-  const age = "25";
-  <h4>This is an element!</h4>; //won't show because it's not being returned
-  return (
-    <h1>
-      {message} my name is {name} {age}
-    </h1>
-  );
+  //props
+  const inputType = "number";
+  const minValue = 5;
+  return <input type={inputType} min={minValue} />;
 }
 //5) Show the component on the screen
 root.render(<App />);
